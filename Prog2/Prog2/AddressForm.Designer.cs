ac03be09623e0f70b6fe6095f8ebf0426f55ae3f
m0kova01 <36977280+m0kova01@users.noreply.github.com>
2019-10-29 10:57:34 -0400

    Add files via upload

---
+++ Prog2/Prog2/AddressForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace UPVApp
+{
+    partial class AddressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.zipLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox1 = new System.Windows.Forms.TextBox();
+            this.addressTextBox2 = new System.Windows.Forms.TextBox();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.zipTextBox = new System.Windows.Forms.TextBox();
+            this.stateComboBox = new System.Windows.Forms.ComboBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(41, 18);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(38, 13);
+            this.nameLabel.TabIndex = 9;
+            this.nameLabel.Text = "Name:";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(31, 44);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(48, 13);
+            this.addressLabel.TabIndex = 10;
+            this.addressLabel.Text = "Address:";
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Location = new System.Drawing.Point(52, 96);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(27, 13);
+            this.cityLabel.TabIndex = 11;
+            this.cityLabel.Text = "City:";
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Location = new System.Drawing.Point(44, 122);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(35, 13);
+            this.stateLabel.TabIndex = 12;
+            this.stateLabel.Text = "State:";
+            // 
+            // zipLabel
+            // 
+            this.zipLabel.AutoSize = true;
+            this.zipLabel.Location = new System.Drawing.Point(54, 149);
+            this.zipLabel.Name = "zipLabel";
+            this.zipLabel.Size = new System.Drawing.Size(25, 13);
+            this.zipLabel.TabIndex = 13;
+            this.zipLabel.Text = "Zip:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(85, 15);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 1;
+            this.nameTextBox.Validating += new System.ComponentModel.CancelEventHandler(this.Name_Validation);
+            this.nameTextBox.Validated += new System.EventHandler(this.Name_Validated);
+            // 
+            // addressTextBox1
+            // 
+            this.addressTextBox1.Location = new System.Drawing.Point(85, 41);
+            this.addressTextBox1.Name = "addressTextBox1";
+            this.addressTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.addressTextBox1.TabIndex = 2;
+            this.addressTextBox1.Validating += new System.ComponentModel.CancelEventHandler(this.Address1_Validating);
+            this.addressTextBox1.Validated += new System.EventHandler(this.Address1_Validated);
+            // 
+            // addressTextBox2
+            // 
+            this.addressTextBox2.Location = new System.Drawing.Point(85, 67);
+            this.addressTextBox2.Name = "addressTextBox2";
+            this.addressTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.addressTextBox2.TabIndex = 3;
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(85, 93);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.cityTextBox.TabIndex = 4;
+            this.cityTextBox.Validating += new System.ComponentModel.CancelEventHandler(this.City_Validating);
+            this.cityTextBox.Validated += new System.EventHandler(this.City_Validation);
+            // 
+            // zipTextBox
+            // 
+            this.zipTextBox.Location = new System.Drawing.Point(85, 146);
+            this.zipTextBox.Name = "zipTextBox";
+            this.zipTextBox.Size = new System.Drawing.Size(100, 20);
+            this.zipTextBox.TabIndex = 6;
+            this.zipTextBox.Validating += new System.ComponentModel.CancelEventHandler(this.Zip_Validating);
+            this.zipTextBox.Validated += new System.EventHandler(this.Zip_Validated);
+            // 
+            // stateComboBox
+            // 
+            this.stateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.stateComboBox.FormattingEnabled = true;
+            this.stateComboBox.Items.AddRange(new object[] {
+            "AL",
+            "AK",
+            "AZ",
+            "AR",
+            "CA",
+            "CO",
+            "CT",
+            "DE",
+            "FL",
+            "GA",
+            "HI",
+            "ID",
+            "IL",
+            "IN",
+            "IA",
+            "KS",
+            "KY",
+            "LA",
+            "ME",
+            "MD",
+            "MA",
+            "MI",
+            "MN",
+            "MS",
+            "MO",
+            "M5",
+            "NE",
+            "NV",
+            "NH",
+            "NJ",
+            "NM",
+            "NY",
+            "NC",
+            "ND",
+            "OH",
+            "OK",
+            "OR",
+            "PA",
+            "RI",
+            "SC",
+            "SD",
+            "TN",
+            "TX",
+            "UT",
+            "VT",
+            "VA",
+            "WA",
+            "WV",
+            "WI",
+            "WY"});
+            this.stateComboBox.Location = new System.Drawing.Point(85, 119);
+            this.stateComboBox.Name = "stateComboBox";
+            this.stateComboBox.Size = new System.Drawing.Size(100, 21);
+            this.stateComboBox.TabIndex = 5;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(44, 183);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 7;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(134, 183);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 8;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Cancel_Mousedown);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // AddressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 230);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.stateComboBox);
+            this.Controls.Add(this.zipTextBox);
+            this.Controls.Add(this.cityTextBox);
+            this.Controls.Add(this.addressTextBox2);
+            this.Controls.Add(this.addressTextBox1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.zipLabel);
+            this.Controls.Add(this.stateLabel);
+            this.Controls.Add(this.cityLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "AddressForm";
+            this.Text = "AddressForm";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.Label zipLabel;
+        internal System.Windows.Forms.TextBox nameTextBox;
+        internal System.Windows.Forms.TextBox addressTextBox1;
+        internal System.Windows.Forms.TextBox addressTextBox2;
+        internal System.Windows.Forms.TextBox cityTextBox;
+        internal System.Windows.Forms.TextBox zipTextBox;
+        internal System.Windows.Forms.ComboBox stateComboBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Prog2/Prog2/AddressForm.cs
@@ -0,0 +1,205 @@
+﻿// Program 2
+// CIS 200-01
+// Fall 2019
+// Due: 10/21/2019
+// Grading ID: M1610
+
+// File: AddressForm.cs
+// The AddressForm is used to add addresses to the upv object.
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace UPVApp
+{
+    public partial class AddressForm : Form
+    {
+        //Precondition:none
+        //Postcondition: the AddressForm is created and the stateComboBox selected index is set to 0
+        public AddressForm()
+        {
+            InitializeComponent();
+            stateComboBox.SelectedIndex = 0;    //Selects the first thing in the state combo box. This ensures something is selected
+        }
+        internal string NameValue // Can be accessed by other classes in same namespace
+        {
+            // Precondition:  None
+            // Postcondition: Text in nameTextBox is returned
+            get { return nameTextBox.Text; }
+
+            // Precondition:  None
+            // Postcondition: Text in nameTextBox is set to specified value
+            set { nameTextBox.Text = value; }
+        }
+        internal string Address1Value // Can be accessed by other classes in same namespace
+        {
+            // Precondition:  None
+            // Postcondition: Text in addressTextBox1 is returned
+            get { return addressTextBox1.Text; }
+
+            // Precondition:  None
+            // Postcondition: Text in addressTextBox1 is set to specified value
+            set { addressTextBox1.Text = value; }
+        }
+        internal string Address2Value // Can be accessed by other classes in same namespace
+        {
+            // Precondition:  None
+            // Postcondition: Text in addressTextBox2 is returned
+            get { return addressTextBox2.Text; }
+
+            // Precondition:  None
+            // Postcondition: Text in addressTextBox2 is set to specified value
+            set { addressTextBox2.Text = value; }
+        }
+        internal string CityValue // Can be accessed by other classes in same namespace
+        {
+            // Precondition:  None
+            // Postcondition: Text in cityTextBox is returned
+            get { return cityTextBox.Text; }
+
+            // Precondition:  None
+            // Postcondition: Text in cityTextBox is set to specified value
+            set { cityTextBox.Text = value; }
+        }
+        internal string StateValue // Can be accessed by other classes in same namespace
+        {
+            // Precondition:  None
+            // Postcondition: Text in stateComboBox is returned
+            get { return stateComboBox.Text; }
+
+            // Precondition:  None
+            // Postcondition: Text in stateComboBox is set to specified value
+            set { stateComboBox.Text = value; }
+        }
+        internal string ZipValue // Can be accessed by other classes in same namespace
+        {
+            // Precondition:  None
+            // Postcondition: Text in zipTextBox is returned
+            get { return zipTextBox.Text; }
+
+            // Precondition:  None
+            // Postcondition: Text in zipTextBox is set to specified value
+            set { zipTextBox.Text = value; }
+        }
+
+        //Precondition: Attempting to change focus from the nameTextBox
+        //Postcondition: errorprovider displays message if the textbox is empty, otherwise focus will change
+        private void Name_Validation(object sender, CancelEventArgs e)
+        {
+            //checks if the textbox is null or empty.
+            if (String.IsNullOrEmpty(nameTextBox.Text))
+            {
+                e.Cancel = true;    //cancels the event
+
+                errorProvider1.SetError(nameTextBox, "Enter a valid name!");    //displays the errorprovider and message
+
+                nameTextBox.SelectAll();    //reselects all
+            }
+        }
+
+        //Precondition: Name_Validation succeeded.
+        //Postcondition: error messages are cleared and focus is allowed to change.
+        private void Name_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(nameTextBox, "");
+        }
+
+        //Precondition: Attempting to change focus from addressTextBox1
+        //Postcondition: errorprovider displays message if the textbox is empty, otherwise focus will change
+        private void Address1_Validating(object sender, CancelEventArgs e)
+        {
+            if (String.IsNullOrEmpty(addressTextBox1.Text))
+            {
+                e.Cancel = true;
+
+                errorProvider1.SetError(addressTextBox1, "Enter a valid address!");
+
+                addressTextBox1.SelectAll();
+            }
+        }
+
+        //Precondition: Address1_Validating succeeded.
+        //Postcondition: error messages are cleared and focus is allowed to change.
+        private void Address1_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(addressTextBox1, "");
+        }
+
+        //Precondition: Attempting to change focus from the cityTextBox
+        //Postcondition: errorprovider displays message if the textbox is empty, otherwise focus will change
+        private void City_Validating(object sender, CancelEventArgs e)
+        {
+            if (String.IsNullOrEmpty(cityTextBox.Text))
+            {
+                e.Cancel = true;
+
+                errorProvider1.SetError(cityTextBox, "Enter a valid city!");
+
+                cityTextBox.SelectAll();
+            }
+        }
+
+        //Precondition: City_Validating succeeded.
+        //Postcondition: error messages are cleared and focus is allowed to change.
+        private void City_Validation(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(cityTextBox, "");
+        }
+
+        //Precondition: Attempting to change focus from the zipTextBox
+        //Postcondition: errorprovider displays message if the textbox doesn't have a valid int, otherwise focus will change
+        private void Zip_Validating(object sender, CancelEventArgs e)
+        {
+            //Checks if it can parse the string into an int.
+            //If not, errorProvider1 is displayed
+            //If yes, continues onto the next check.
+            if (!int.TryParse(zipTextBox.Text, out int zip))
+            {
+                e.Cancel = true;
+
+                errorProvider1.SetError(zipTextBox, "Enter an integer!");
+
+                zipTextBox.SelectAll();
+            }
+            else
+            {
+                //Last check to make sure int is a valid zip between 0-99999.
+                //If not, errorProvider 1 is displayed
+                //If yes, focus is allowed to change.
+                if (zip < 0 && zip > 99999)
+                {
+                    e.Cancel = true;
+
+                    errorProvider1.SetError(zipTextBox, "Enter a valid zip code!");
+
+                    zipTextBox.SelectAll();
+                }
+            }
+        }
+
+        //Precondition: Zip_Validating succeeded.
+        //Postcondition: error messages are cleared and focus is allowed to change.
+        private void Zip_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(zipTextBox, "");
+        }
+
+        // Precondition:  okButton is clicked
+        // Postcondition: If all controls on form validate, InputBox is dismissed with OK result
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            //Checks if all children have finished validation and validated events.
+            if (this.ValidateChildren())
+                this.DialogResult = DialogResult.OK;
+        }
+
+        // Precondition:  cancelButton is clicked
+        // Postcondition: If left-click, InputBox is dismissed with Cancel result
+        private void Cancel_Mousedown(object sender, MouseEventArgs e)
+        {
+            //Checks if the left mouse button was used.
+            if (e.Button == MouseButtons.Left)
+                this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
